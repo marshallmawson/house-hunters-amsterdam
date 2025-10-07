7bd2acf6df0de511bbe7f7632d15b7b1763d0999
marshallmawson <marshall.mawson@gmail.com>
2025-10-07 14:08:02 +0200

    errors fixed

--- frontend/huis-hunters-frontend/src/react-icons.d.ts
+++ frontend/huis-hunters-frontend/src/react-icons.d.ts
@@ -1,9 +1,10 @@
 declare module 'react-icons/fa' {
-  import { IconType } from 'react-icons';
-  export const FaBed: IconType;
-  export const FaBath: IconType;
-  export const FaRulerCombined: IconType;
-  export const FaLeaf: IconType;
-  export const FaBolt: IconType;
-  export const FaCalendarAlt: IconType;
+  import { IconBaseProps } from 'react-icons';
+  import { ElementType } from 'react';
+  export declare const FaBed: ElementType<IconBaseProps>;
+  export declare const FaBath: ElementType<IconBaseProps>;
+  export declare const FaRulerCombined: ElementType<IconBaseProps>;
+  export declare const FaLeaf: ElementType<IconBaseProps>;
+  export declare const FaBolt: ElementType<IconBaseProps>;
+  export declare const FaCalendarAlt: ElementType<IconBase.Props>;
 }
